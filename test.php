cf1ef7280aebcb9eb55f62f3e63baffa6c466bd4
Andreas Ahlen <nordlage37@gmail.com>
2022-06-20 13:18:49 +0200

    - fixes to style and responsive layout refs #0

--- test.php
+++ test.php
@@ -5,28 +5,35 @@
 <head>
 <title>Calendar</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="keywords" content="">
- <link rel="stylesheet" href="styles/gridlayout.css">
+<link rel="stylesheet" href="styles/gridlayout.css">
+ <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
 
 <!-- page content goes here -->
-<div id="calendar" class="wrapper">
+<p class="paligncenter">
+
 <?php
     $localizedDaysOfWeek = array("So", "Mo", "Di", "Mi", "Do", "Fr", "Sa");
     $localizedMonths = array("", "Januar", "Februar", "Maerz", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember");
     $day = 1;
-    $month = 7;
+    $month = 12;
     $year = 2022;
     $diff = 0;
     $diffEnd = 0;
     $weekStartOnMonday = 1;
-    $daysPerMonth = cal_days_in_month(CAL_GREGORIAN,$month,$year);
+    //$daysPerMonth = cal_days_in_month(CAL_GREGORIAN,$month,$year);
+    $daysPerMonth = intval(date('t', strtotime("$year-$month-1")));
     $maxTiles = 49 + 1; // 6 x 7 days + 1 (for loop)
     $color = "";
     $diffIndex = 0;
+
+    echo "<div id='calendar' class='wrapper'>";
 
     for ($index = 0; $index < count($localizedDaysOfWeek); $index++)
     {
@@ -47,7 +54,8 @@
             $diffIndex = 0;
         }
 
-        echo "<span id='$localizedDaysOfWeek[$diffIndex]' style='background-color:$color'>$localizedDaysOfWeek[$diffIndex]</span>";
+        echo "<span id='$localizedDaysOfWeek[$diffIndex]' class='tileWeekDaysAlignment' style='background-color:$color'>$localizedDaysOfWeek[$diffIndex]</span>";
+
     }
 
     for ($index = 1; $index < $maxTiles; $index++)
@@ -57,6 +65,7 @@
         if ($index == 1)
         {
             $diffEnd = $dayOfWeek;
+            $diffStart = $diffEnd;
         }
         if ($dayOfWeek == 0)
         {
@@ -82,18 +91,21 @@
         {
             if ($dayOfWeek > 0)
             {
-                echo "<span id=$dateString style='background-color:$color'>$day ($dayOfWeek)</span>";
+                echo "<span id='$dateString' class='tileMonthDayAlignment' style='background-color:$color'>$day ($dayOfWeek)</span>" . PHP_EOL;
             }
             else
             {
-                echo "<span id=$dateString style='background-color:$color'>$day ($dayOfWeek)</span>";
+                echo "<span id='$dateString' class='tileMonthDayAlignment' style='background-color:$color'>$day ($dayOfWeek)</span>" . PHP_EOL;
             }
             $day++;
         }
+
     }
+    echo "</div>";
 ?>
-</div>
 
+</p>
 </body>
-
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
